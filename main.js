453703aa85492fa40b19984188f6905a9600404b
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-03-17 17:36:31 -0600

    Created header with contact info, skills, education, and contact form

---
+++ main.js
@@ -0,0 +1,8 @@
+function showPhoneNumber() {
+    var phone = document.getElementById("phone");
+    if (phone.style.display === "none") {
+        phone.style.display = "block";
+    } else {
+        phone.style.display = "none";
+    }
+}
