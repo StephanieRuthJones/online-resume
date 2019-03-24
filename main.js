b1fd0990afcd2d68ded289949a6a7223dd92905e
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-03-24 16:02:28 -0600

    Updated projects

--- main.js
+++
@@ -1,8 +0,0 @@
-function showPhoneNumber() {
-    var phone = document.getElementById("phone");
-    if (phone.style.display === "none") {
-        phone.style.display = "block";
-    } else {
-        phone.style.display = "none";
-    }
-}
---
+++ public/main.js
@@ -0,0 +1,9 @@
+const showPhoneNumber = () => {
+    var element = document.getElementById("phone");
+    element.classList.toggle("hidden");
+}
+
+const showEmail = () => {
+    var element = document.getElementById("email");
+    element.classList.toggle("hidden");
+}
